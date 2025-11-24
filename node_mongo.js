e04df5b0f83d3714127ecdcaf07c76b38cd5a466
markfediotufts <mark.fedio@tufts.edu>
2025-11-24 17:39:46 -0500

    Update node_mongo.js

--- node_mongo.js
+++ node_mongo.js
@@ -1,9 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = mongodb+srv://test:test@cluster0.xzre7sj.mongodb.net/?appName=Cluster0
+const url = "mongodb+srv://test:test@cluster0.xzre7sj.mongodb.net/?appName=Cluster0"
 MongoClient.connect(url, function(err, db) {
 if(err) { return console.log(err); return;}
-var dbo = db.db("library");
-var collection = dbo.collection('bks');
+var dbo = db.db("adoption_center");
+var collection = dbo.collection('pets');
 console.log("Success!");
 db.close();
 });
